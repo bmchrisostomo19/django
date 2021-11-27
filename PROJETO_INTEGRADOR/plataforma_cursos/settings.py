c49436b296c17d83937d23e96f6f2c3fd38ef073
bmchrisostomo19 <2000763@aluno.univesp.com.br>
2021-11-27 13:18:21 -0300

    fix(django) : termino do projeto web

--- PROJETO_INTEGRADOR/plataforma_cursos/settings.py
+++ PROJETO_INTEGRADOR/plataforma_cursos/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,5 +137,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
+
